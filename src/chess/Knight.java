9535a7e7744d48dc6c9262232a45a00cfa788bd9
xtreme0ninja <jamespaterson87@gmail.com>
2014-12-31 17:40:47 -0500

    Added getLegalMoves to knight

--- src/chess/Knight.java
+++ src/chess/Knight.java
@@ -24,4 +24,44 @@ public class Knight extends Piece{
         super.setImage(chessImage);
     }
     
+    @Override
+    public Position[] getLegalMoves(Board state, Position pieceLocation){
+        Position[] moves = new Position[8];
+        int currentX = pieceLocation.x;
+        int currentY = pieceLocation.y;
+        
+        if(currentX >= 2 && currentY >= 1){
+            moves[0] = new Position(currentX-2, currentY-1);
+        }
+        
+        if(currentX >=1 && currentY >= 2){
+            moves[1] = new Position(currentX-1, currentY-2);
+        }
+        
+        if(currentX <= 6 && currentY >= 2){
+            moves[2] = new Position(currentX+1, currentY-2);
+        }
+        
+        if(currentX <= 5 && currentY >= 1){
+            moves[3] = new Position(currentX+2, currentY-1);
+        }
+        
+        if(currentX <= 5 && currentY <= 6){
+            moves[4] = new Position(currentX+2, currentY+1);
+        }
+        
+        if(currentX <= 6 && currentY <= 5){
+            moves[5] = new Position(currentX+1, currentY+2);
+        }
+        
+        if(currentX >= 1 && currentY <= 5){
+            moves[6] = new Position(currentX-1, currentY+2);
+        }
+        
+        if(currentX >= 2 && currentY <= 6){
+            moves[7] = new Position(currentX-2, currentY+1);
+        }
+        
+        return moves;
+    }
 }
